e125c650ee8f8643a749af644d86ed0bc485c715
Dylan Neve <dylanneve1@gmail.com>
2024-01-30 22:17:07 +0000

    zygisk: implement new spooflist method for preSpecialize
    d

--- zygisk/module/jni/module.cpp
+++ zygisk/module/jni/module.cpp
@@ -74,6 +74,23 @@ private:
         return bytesReceived;
     }
 
+    bool appShouldSpoof (const std::string& process) {
+        bool ret = false;
+        int len = 1;
+
+        std::vector<std::string> spooflist = {
+            "com.google.android.GoogleCamera"
+        };
+
+        for (int i = 0; i < len; i++) {
+            if (process == spooflist[i]) {
+                ret = true;
+            }
+        }
+
+        return ret;
+    }
+
     void loadPayload() {
         auto fd = api->connectCompanion();
 
@@ -84,7 +101,7 @@ private:
     }
 
     void preSpecialize(const std::string& process) {
-        if (process == "com.google.android.GoogleCamera") {
+        if (appShouldSpoof(process)) {
             // Load the payload, but don't inject it yet until after specialization
             // Otherwise, specialization fails if any code from the payload still happens to be
             // running
