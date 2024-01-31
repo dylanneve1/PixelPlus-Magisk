316491242165a9eba4d451ed7a6e4d9cee75ca77
Dylan Neve <dylanneve1@gmail.com>
2024-01-31 14:11:11 +0000

    zygisk: update spoof list

--- zygisk/module/jni/module.cpp
+++ zygisk/module/jni/module.cpp
@@ -76,11 +76,13 @@ private:
 
     bool isWhitelisted (const std::string& process) {
         bool ret = false;
-        int len = 5;
+        int len = 7;
 
         std::vector<std::string> whitelist = {
             "com.google.android.apps.photos",
             "com.google.android.as",
+            "com.google.android.gms",
+            "com.google.android.gsf",
             "com.google.android.settings.intelligence",
             "com.android.settings",
             "com.google.android.GoogleCamera"
